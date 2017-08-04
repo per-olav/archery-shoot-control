0123bc6c4564f55e8ee3f3fc9996e6ad44855889
per-olavr <per.olav.rusas@gmail.com>
2017-08-04 08:18:48 +0200

    DisplayLED project added

---
+++ DisplayLED/.ino.cpp
@@ -0,0 +1,26 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-08-04 08:17:52
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+#include <FastLED.h>
+void setup() ;
+void loop() ;
+void updateCommand(struct Command *cP) ;
+void resetCommand(struct Command *cP) ;
+void updateColor() ;
+bool updateIsShooting() ;
+bool updateShootTimer() ;
+bool updateWaitTimer() ;
+void updateDisplay() ;
+void showDigit(int position, int digit) ;
+void clearDigit(int position) ;
+void showThreeDigitNumber(int number) ;
+
+#include "DisplayLED.ino"
+
+
+#endif
