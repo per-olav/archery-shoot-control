c0b1c8b3ebb7587620cda67092a5cd827e7a5e84
per-olav <per.olav.rusas@gmail.com>
2017-12-26 19:13:57 +0100

    Div

--- shootControlPanel/Release/.ino.cpp.d
+++ shootControlPanel/Release/.ino.cpp.d
@@ -1,45 +1,45 @@
-.ino.cpp.o: ../.ino.cpp \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SoftwareSerial\src/SoftwareSerial.h \
- C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
- ../ReceivingCommand.h ../ShootControlPanel.ino
+.ino.cpp.o: ..\.ino.cpp \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ ..\ReceivingCommand.h ..\ShootControlPanel.ino
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SoftwareSerial\src/SoftwareSerial.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SoftwareSerial\src/SoftwareSerial.h:
 
-C:\eclipse\java-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
+C:\eclipse-neon\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
 
-../ReceivingCommand.h:
+..\ReceivingCommand.h:
 
-../ShootControlPanel.ino:
+..\ShootControlPanel.ino:
