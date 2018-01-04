7c8d85aa68a6260b2e133a36001189d7ce6647f7
per-olav <per.olav.rusas@gmail.com>
2018-01-04 19:08:31 +0100

    IRRemote

--- shootControlPanel/.ino.cpp
+++ shootControlPanel/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-12-29 10:25:30
+//This file has been generated on 2018-01-02 21:57:59
 
 #include "Arduino.h"
 #include <Arduino.h>
